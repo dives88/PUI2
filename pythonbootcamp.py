b290d914f38d96426993080fc19266ecd6e30412
Davey Ives <dhi211@compute.cusp.nyu.edu>
2017-11-08 16:21:09 -0500

    adding all back

---
+++ pythonbootcamp.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+import datetime 
+
+#def printcontent(timeNow, name = "Davey")
+
+
+
+# sys.argv[1] to = Davey
+
+datetime.datetime.now()
+if datetime.datetime.now():
+    print "Good Morning" +  sys.argv[1] 
+else:
+    print "Good Afternoon" +  sys.arg[1]
+
+
